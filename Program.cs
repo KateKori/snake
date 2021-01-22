18a76e3ddbbc77512adbb7a3e1ac550839ac6bfa
KateKori <korikorit@gmail.com>
2021-01-22 21:00:55 +0300

    hello world

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Osnovy_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
