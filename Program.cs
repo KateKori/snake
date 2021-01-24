ae509b3ed6cb8a74f667f8696aaf30c963aca2b4
KateKori <korikorit@gmail.com>
2021-01-24 14:43:58 +0300

    List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Osnovy_OOP
 {
@@ -7,11 +8,33 @@ namespace Osnovy_OOP
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+           // p1.Draw();
       
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-  
+           // p2.Draw();
+
+           /* List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }*/
+
+
+            List<Point> pList = new List<Point>();
+            {
+                pList.Add(p1);
+                pList.Add(p2);
+            }
+
+            foreach (Point a in pList)
+            {
+                a.Draw();
+            }
+
             Console.ReadLine();
         }    
     }
